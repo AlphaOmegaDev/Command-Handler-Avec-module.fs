ddd982a89ec6ab8d0e15f730d21976654d5569b3
AlphaNya <75203244+AlphaOmegaDev@users.noreply.github.com>
2021-01-02 14:44:35 +0100

    Delete ready.js

--- ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = async(client, data) => {
-    client.user.setActivity(`Bot en chargement`, {
-        type: "STREAMING",
-        url: "https://www.twitch.tv/nezukochan"
-      });
-}
